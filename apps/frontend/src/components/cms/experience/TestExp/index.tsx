f47025c033f6399e4c0e09d675c8b4ad8ed8049e
Arun Kumar <arun.k@icreon.com>
2025-07-31 18:50:53 +0530

    build fixes

--- apps/frontend/src/components/cms/experience/TestExp/index.tsx
+++ apps/frontend/src/components/cms/experience/TestExp/index.tsx
@@ -1,6 +1,6 @@
 import { type OptimizelyNextPage as CmsComponent } from "@remkoj/optimizely-cms-nextjs";
 import { getFragmentData } from "@/gql/fragment-masking";
-import { ExperienceDataFragmentDoc, CompositionDataFragmentDoc, TestExpDataFragmentDoc, type TestExpDataFragment } from "@/gql/graphql";
+import { ExperienceDataFragmentDoc, TestExpDataFragmentDoc, type TestExpDataFragment } from "@/gql/graphql";
 import { OptimizelyComposition, isNode, CmsEditable } from "@remkoj/optimizely-cms-react/rsc";
 import { getSdk } from "@/gql"
 
@@ -8,10 +8,10 @@ import { getSdk } from "@/gql"
  * TestExp
  * Test Exp
  */
-export const TestExpExperience : CmsComponent<TestExpDataFragment> = ({ data, ctx }) => {
-    const composition = getFragmentData(CompositionDataFragmentDoc, getFragmentData(ExperienceDataFragmentDoc, data)?.composition)
+export const TestExpExperience: CmsComponent<TestExpDataFragment> = ({ data, ctx }) => {
+    const composition = getFragmentData(ExperienceDataFragmentDoc, data)?.composition;
     return <CmsEditable as="div" className="mx-auto px-2 container" cmsFieldName="unstructuredData" ctx={ctx}>
-        { composition && isNode(composition) && <OptimizelyComposition node={composition} /> }
+        {composition && isNode(composition) && <OptimizelyComposition node={composition} />}
     </CmsEditable>
 }
 TestExpExperience.displayName = "TestExp (Experience/TestExp)"
--- apps/frontend/src/components/cms/nodes/column/fontsize/index.tsx
+++ apps/frontend/src/components/cms/nodes/column/fontsize/index.tsx
@@ -1,8 +1,8 @@
 import { extractSettings, type CmsLayoutComponent } from "@remkoj/optimizely-cms-react/rsc";
 
-export const fontsize : CmsLayoutComponent<{}> = ({ contentLink, layoutProps, children }) => {
-    const layout = extractSettings(layoutProps);
-    return (<div className="vb:column vb:column:fontsize">{ children }</div>);
+export const fontsize: CmsLayoutComponent<{}> = ({ children }) => {
+    //const layout = extractSettings(layoutProps);
+    return (<div className="vb:column vb:column:fontsize">{children}</div>);
 }
 
 export default fontsize;
